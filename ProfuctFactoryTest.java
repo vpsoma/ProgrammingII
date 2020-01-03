d2c7f59eb2eaaf3db18a6da1964c807f35a7fb0a
EirhannaPapoutsi <irianna_pap@hotmail.gr>
2020-01-03 19:01:39 +0200

    Adding the JUnit Test of class ProductFactory

---
+++ ProfuctFactoryTest.java
@@ -0,0 +1,30 @@
+package ProgrammingII;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProfuctFactoryTest {
+	
+	private ProductFactory productFactory;
+	
+	@BeforeEach 
+	public void setUp() {
+		productFactory = new ProductFactory();
+	}
+
+	@Test
+	public void findSellPeriodTest() {
+		
+		//Given
+		int quantity = 40;
+		double costSold = 576;
+		double expected = 360 / (costSold / quantity);
+		
+		//When
+		double result = productFactory.findSellPeriod(quantity, costSold);
+		
+		//Then
+		assertEquals(result, expected);
+	}
+}
