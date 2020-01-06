1dfba7090a7ff58f584361644fbe49ef1f4d30e8
EirhannaPapoutsi <irianna_pap@hotmail.gr>
2020-01-06 18:34:12 +0200

    Making some corrections

--- Products.java
+++ Products.java
@@ -32,9 +32,10 @@ public class Products {
 	    	 return z;
 	  } 
     	 
+	  ArrayList<Integer> samecode = new ArrayList<Integer>(); /*contains days passed*/
+	  ArrayList<Integer> samecodeindex = new ArrayList<Integer>();
+		
 	 public void purchase(int code, int quantity) {
-		ArrayList<Integer> samecode = new ArrayList<Integer>(); /*contains days passed*/
-		ArrayList<Integer> samecodeindex = new ArrayList<Integer>();
 		int sum = 0;
 		for (int i = 0; i < Arraylist.size() ; i++) { /*arraylist of product class*/
 			if (Arraylist.get(i).getCode().equals(code))
@@ -49,8 +50,8 @@ public class Products {
 			boolean sorted = false;
 			while (!sorted) {
 				sorted = true;
-				for (int i = 0; i<samecode.size(); i++) {
-					if (samecode.get(i)<samecode.get(i + 1)) {
+				for (int i = 0; i < samecode.size(); i++) {
+					if (samecode.get(i) < samecode.get(i + 1)) {
 						 int temp1 = samecode.get(i);
 						 samecode.get(i) = samecode.get(i + 1);
 						 samecode.get(i + 1)= temp;
@@ -71,12 +72,12 @@ public class Products {
 	
 	 public void quantity(int quantity) {
 		if (quantity < Arraylist.get(samecodeindex(i)).getQuantity())
-			Arraylist.get(samecodeindex(i)).setQuantity(Arraylist.get(samecodeindex(i)).getQuantity - quantity);
+			Arraylist.get(samecodeindex(i)).setQuantity(Arraylist.get(samecodeindex(i)).getQuantity() - quantity);
 		else if (quantity == Arraylist.get(samecodeindex(i)).getQuantity())
 			Arraylist.remove(samecodeindex(i));
 		else {
-			Arraylist.remove(samecodeindex(i));
 		    quantity = quantity - Arraylist.get(samecodeindex(i)).getQuantity();
+		    Arraylist.remove(samecodeindex(i));
 	    }
 	}
 }
