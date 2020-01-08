d3d07aef506c48bcfa9f16cec0888da85e64c5f2
EirhannaPapoutsi <irianna_pap@hotmail.gr>
2020-01-08 17:53:20 +0200

    Found a better way to calculate the period between two dates

--- Products.java
+++ Products.java
@@ -1,70 +1,19 @@
 import java.util.ArrayList;
 
 public class Products {
-	 public int findDays(int day, int month, int year) {
-    	 Date date = new Date();
-    	 SimpleDateFormat sdfd = new SimpleDateFormat("dd");
-    	 String ds = sdfd.format(date); // ds = dayString
-    	 int di = Integer.parseInt(ds); // di = dayint μετατρέπει String σε int
-    	 SimpleDateFormat sdfm = new SimpleDateFormat("MM");
-    	 String ms = sdfm.format(date); // ms = monthString
-    	 int mi = Integer.parseInt(ms); // mi = monthint μετατρέπει String σε int
-    	 SimpleDateFormat sdfy = new SimpleDateFormat("yyyy");
-    	 String ys = sdfy.format(date); // ys = yearString
-    	 int yi = Integer.parseInt(ys); // yi = yearint μετατρέπει String σε int
-    	 int diff=0;
-	 }
-	 
-	 if (di >= day && mi > month + 1 && yi == year) {
-		 diff = di - day;
-		 for (int x = month; x > mi; x++) {
-			 diff += dom(month, year);
-		 }
-	 } else if (di >= day && mi > month + 1 && yi > year) {
-		 diff = di - day;
-		 if (yi == year + 1) {
-			 if (yi % 400 == 0 || (yi % 100 != 0 && yi % 4 != 0)) {
-				 diff += 366;
-			 } else {
-				 diff += 365;
-			 }
-		 }
-		 for (int x = month; x > mi; x ++) {
-			 diff += dom(x, year);
-		 }
-		 diff += di;
-	 } else if (di < day && mi > month + 1 && yi == year + 1) {
-		 diff = diff += dom(month, year);
-		 for (int i = month + 1; i <= 12; i++) {
-			 diff += dom(i, year);
-		 }
-		 if (mi == 1) {
-			 diff += di;
-		 } else {
-			 for (int i = 1; i <= mi; i++) {
-				 diff += di;
-			 }
-		 }
-	 
-	  public int dom(int m, int y) { // dom=DaysOfMonth
-	    	 int z;
-	    	 if ((m % 2 != 0 && m >= 1 && m <= 7) || (m % 2 == 0 && m > 7 && m <= 12)) {
-	    		 z = 31;
-	    	 }
-	    	 else if (m == 2) {
-	    		 if (y % 400 == 0 || (y % 100 != 0 && y % 4 != 0)) {
-	    			 z = 29;
-	    		 } else {
-	    			 z = 28;
-	    		 }
-	    	 } else {
-	    		 z = 30;
-	    	 }
-	    	 return z;
-	  } 
+	
+	protected Integer findDifferencebetweenTwoDates(Product input) {
+		LocalDate now = LocalDate.now(); // Finds the current date
+	    LocalDate dateThen = LocalDate.of(input.getYear(), input.getMonth(), input.getMonth()); // Changes the date of the input into an object of LocalDate reference type
+	    /* Calls the static method between in order to estimate the days passed between the two parameters given 
+	     * and saves it to the variable named period 
+	     */
+	    Period period = Period.between(now, dateThen); 
+	    return period.getDays();
+	}
     	 
-	  ArrayList<Integer> samecode = new ArrayList<Integer>(); // contains days passed
-	  ArrayList<Integer> samecodeindex = new ArrayList<Integer>();
+	ArrayList<Integer> samecode = new ArrayList<Integer>(); // contains days passed
+	ArrayList<Integer> samecodeindex = new ArrayList<Integer>();
 		
 	 public void purchase(int code, int quantity) {
 		int sum = 0;
