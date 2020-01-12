88a7f2717e3e08af95b4cca6898a3daedd5ad94e
mariaspastef <56541286+mariaspastef@users.noreply.github.com>
2020-01-12 21:38:18 +0200

    Corrections

--- Gifts.java
+++ Gifts.java
@@ -1,7 +1,10 @@
-package com.progII.gifthub;
-
 import java.util.Scanner;
-import java.util.Iterator;
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -10,146 +13,157 @@ import java.util.Comparator;
 /**
  * 
  * @author Maria Aspasia Stefadourou.
- *
+ * 
  */
 
 public class Gifts {
-	private int numberOfPoductsAsGifts = 0;
-	private int numberOfPossibleGifts;
-	Products c = new Products();
-<<<<<<< HEAD
-	ArrayList <Product> leftOverProducts = (ArrayList<Product>) c.createList();
-	ArrayList <NewPurchasesSeparation> offered = new ArrayList<NewPurchasesSeparation>();
-=======
-	ArrayList<Product> productsPassedTheSellPeriod = (ArrayList<Product>) c.createListofProductsPassedTheSellPeriod();
-	Customer c1 = new Customer();
->>>>>>> 7369311192bca5c0bec635d85e50d1a80b86f5fe
-	private int sizeOfnewoffered = Customer.newoffered.size();
+		private int numberOfPoductsAsGifts; 
+		private int numberOfPossibleGifts;
+		int numberOfGifts;
+		Products c = new Products();
+		Customer objectOfCustomerClass = new Customer();
+		ArrayList<Product> productsPassedTheSellPeriod = c.createListofProductsPassedTheSellPeriod();
+		int sizeOfnewoffered = Customer.newoffered.size();
+	
+		/**
+		 * find how many products have surpassed their sell period and can be given as presents to the customers.
+		 * @param input a list with the products over the sell period
+		 * @return the number of products over the sell period
+		 */
+		public int findNumberOfProductsAsGifts(ArrayList<Product> productsPassedTheSellPeriod) {
+			// A loop getting every product over the sell period.
+			for (int i = 0; i < productsPassedTheSellPeriod.size(); i++) {
+				Product prod = productsPassedTheSellPeriod.get(i);
+				numberOfPoductsAsGifts = numberOfPoductsAsGifts + prod.getQuantity(); //variable that contains the total number of products over the sell period.
+			}
+			return numberOfPoductsAsGifts;
+		}
+		
+		/**
+		 * calculate the maximum number of gifts based on the products that can be
+		 * gifted and the number of customers that deserve to be gifted a product.
+		 * @param input the size of the list that contains the customers that deserve to receive a present
+		 * @return the maximum number of gifts that can be made 
+		 */
+		public int findNumberOfGifts(int sizeOfnewoffered) {
+			findNumberOfProductsAsGifts(productsPassedTheSellPeriod);
+			//
+			if (numberOfPoductsAsGifts <= sizeOfnewoffered) {
+				numberOfPossibleGifts = numberOfPoductsAsGifts;
+			} else {
+				numberOfPossibleGifts = sizeOfnewoffered;
+			}
+		 	return numberOfPossibleGifts;
+		}
+		
+		/**
+		 * ask the customer how many gifts they want to make.
+		 * @return the number of gifts that are to be made 
+		 */
+		public int askNumberOfGifts() {
+			findNumberOfGifts(sizeOfnewoffered);
+			int numberOfGifts = Integer.parseInt((JOptionPane.showInputDialog("You can make up to " + numberOfPossibleGifts +
+										" gifts to your most valuable customers. How many gifts do you wish to make?")));
+			
+			return numberOfGifts;
+		}
+		
+		/**
+		 * Sort in descending order the list of the products that can be gifted based on their price.
+		 * @param input a list with the products over the sell period
+		 * @return the list sorted in descending order based in the price 
+		 */
+		public void sortMyListBasedOnThePrice(ArrayList<Product> productsPassedTheSellPeriod) {
+			 // Sort (based on the price)
+			Collections.sort(productsPassedTheSellPeriod, new Comparator<Product>() {
+				public int compare(Product one, Product other) {
+					return other.getPrice().compareTo(one.getPrice());
+				}
+			});
+		}
+		
+		
+		/**
+		 * Sort in descending order the list of customers that can receive a gift based on their total fees.
+		 * @param input a list with the costumers that deserve to be gifted
+		 * @return the list sorted in descending order based on their total fees
+		 */
+		public void sortMyListBasedOnTheTotalFees (ArrayList<NewPurchasesSeparation> newoffered) {
+			 // Sort (based on the total fees)
+			Collections.sort(newoffered, new Comparator<NewPurchasesSeparation>() {
+				public int compare(NewPurchasesSeparation one, NewPurchasesSeparation other) {
+					return other.getNewfees().compareTo(one.getNewfees());
+				}
+			});
+		}
 
-<<<<<<< HEAD
-	/**
-	 * find how many products have surpassed their aimed period of being sold and can be 
-	 * given as presents to the customers.
-	 */
-	for (int i = 0; i < leftOverProducts.size() ; i++) {
-		Product prod = leftOverProducts.get(i);
-		numberOfPoductsAsGifts = numberOfPoductsAsGifts + prod.getQuantity();
-	}
-	
-	/*
-	 * calculate the possible number of gifts based on the products that can be gifted and the 
-	 * number of customers that deserve to take an offer.
-	 */
-	
-	if (numberOfPoductsAsGifts <= sizeOfnewoffered) {
-		numberOfPossibleGifts = numberOfPoductsAsGifts;
-	} else {
-		numberOfPossibleGifts = sizeOfnewoffered;
-	}
-	Scanner sc = new Scanner (System.in);
-	System.out.println("You can make up to" + numberOfPossibleGifts + "gifts to your most valuable customers. How many gifts do you wish to make?");
-	int numberOfGifts = sc.nextInt();
-	
-	/*
-	 * Sort in descending order the list of the products that can be gifted based on their price.
-	 */
-	Collections.sort(leftOverProducts, new Comparator<Product>() {
-	    public int compare(Product one, Product other) {
-	        return other.getPrice().compareTo(one.getPrice());
-	    }
-	});
-	
-	/*
-	 * Sort in descending order the list of customers that can receive a gift based on their total fees.
-	 */
-	Collections.sort(newoffered, new Comparator<NewPurchasesSeparation>() {
-	    public int compare(NewPurchasesSeparation one, NewPurchasesSeparation other) {
-	        return other.getNewFees().compareTo(NewPurchasesSeparation.getNewFees());
-	    }
-	});	
-=======
-	public InfoMail findGiftsReceivers(ArrayList<NewPurchasesSeparation> newoffered,
-			ArrayList<Product> productsPassedTheSellPeriod) {
 
 		/**
-		 * find how many products have surpassed their aimed period of being sold and
-		 * can be given as presents to the customers.
-		 */
-		for (int i = 0; i < productsPassedTheSellPeriod.size(); i++) {
-			Product prod = productsPassedTheSellPeriod.get(i);
-			numberOfPoductsAsGifts = numberOfPoductsAsGifts + prod.getQuantity();
-		}
-
-		/*
-		 * calculate the possible number of gifts based on the products that can be
-		 * gifted and the number of customers that deserve to take an offer.
-		 */
-
-		if (numberOfPoductsAsGifts <= sizeOfnewoffered) {
-			numberOfPossibleGifts = numberOfPoductsAsGifts;
-		} else {
-			numberOfPossibleGifts = sizeOfnewoffered;
-		}
-		Scanner sc = new Scanner(System.in);
-		System.out.println("You can make up to" + numberOfPossibleGifts
-				+ "gifts to your most valuable customers. How many gifts do you wish to make?");
-		int numberOfGifts = sc.nextInt();
-
-		/*
-		 * Sort in descending order the list of the products that can be gifted based on
-		 * their price.
-		 */
-		Collections.sort(productsPassedTheSellPeriod, new Comparator<Product>() {
-			public int compare(Product one, Product other) {
-				return other.getPrice().compareTo(one.getPrice());
-			}
-		});
-
-		/*
-		 * Sort in descending order the list of customers that can receive a gift based
-		 * on their total fees.
-		 */
-		Collections.sort(newoffered, new Comparator<NewPurchasesSeparation>() {
-			public int compare(NewPurchasesSeparation one, NewPurchasesSeparation other) {
-				return other.getNewFees().compareTo(NewPurchasesSeparation.getNewFees());
-			}
-		});
->>>>>>> 7369311192bca5c0bec635d85e50d1a80b86f5fe
-
-		String namesOfCustomersForGifts[] = new String[numberOfGifts];
-		String mailsOfCustomersForGifts[] = new String[numberOfGifts];
-		String namesOfProductsAsGifts[] = new String[numberOfGifts];
-
-		/*
 		 * Match the customers that are to receive a gift with the products that will be
-		 * gifted to them according to the customers' expenses.
+		 * gifted to them according to the customers' expenses and the products' prices
+		 * @param input a list with the products over the sell period sorted in descending order
+		 * @param input a list with the costumers that deserve to be gifted sorted in descending order
+		 * @return an object of type InfoMail
 		 */
-		final int INDEX = 0;
-		final Product ProductToBeGifted = productsPassedTheSellPeriod.get(INDEX);
-		for (int i = 0; i <= numberOfGifts; i++) {
-			if (ProductToBeGifted.getQuantity() > 1) {
-				namesOfProductsAsGifts[i] = ProductToBeGifted.getName();
-				ProductToBeGifted.setQuantity(ProductToBeGifted.getQuantity() - 1);
-			} else if (ProductToBeGifted.getQuantity() == 1) {
-				namesOfProductsAsGifts[i] = ProductToBeGifted.getName();
-				productsPassedTheSellPeriod.remove(INDEX);
+		public InfoMail findGiftsReceivers(ArrayList<NewPurchasesSeparation> newoffered,
+			ArrayList<Product> productsPassedTheSellPeriod) {
+			sortMyListBasedOnThePrice(productsPassedTheSellPeriod);
+			sortMyListBasedOnTheTotalFees(newoffered);
+			askNumberOfGifts();
+			String namesOfCustomersForGifts[] = new String[numberOfGifts];
+			String mailsOfCustomersForGifts[] = new String[numberOfGifts];
+			String namesOfProductsAsGifts[] = new String[numberOfGifts];
+			final int INDEX = 0;
+			Product ProductToBeGifted = productsPassedTheSellPeriod.get(INDEX);
+			for (int i = 0; i < numberOfGifts; i++) {
+				if (ProductToBeGifted.getQuantity() > 1) {
+					namesOfProductsAsGifts[i] = ProductToBeGifted.getName();
+					ProductToBeGifted.setQuantity(ProductToBeGifted.getQuantity() - 1);
+				} else if (ProductToBeGifted.getQuantity() == 1){
+					namesOfProductsAsGifts[i] = ProductToBeGifted.getName();
+					productsPassedTheSellPeriod.remove(INDEX);
+				}
+				namesOfCustomersForGifts[i] = newoffered.get(i).getNewName();
+				mailsOfCustomersForGifts[i] = newoffered.get(i).getNewMail();
+				updateOfferedInDataBase(Customer.newoffered.get(i).getNewName(),Customer.newoffered.get(i).getNewMail());
 			}
-			namesOfCustomersForGifts[i] = newoffered.get(i).getNewName();
-			mailsOfCustomersForGifts[i] = newoffered.get(i).getNewMail();
-			NewPurchasesSeparation a = new NewPurchasesSeparation(newoffered.get(i).getNewName(),
-					newoffered.get(i).getNewMail(), newoffered.get(i).getNewFees());
-			c1.getOffered().add(a);
+			InfoMail object = new InfoMail(namesOfCustomersForGifts, mailsOfCustomersForGifts, namesOfProductsAsGifts);
+			return object;
 		}
-<<<<<<< HEAD
-		namesOfCustomersForGifts [i] = newoffered.get(i).getNewName();
-		mailsOfCustomersForGifts [i] = newoffered.get(i).getNewMail();
-		NewPurchasesSeparation a = new NewPurchasesSeparation(newoffered.get(i).getNewName(),newoffered.get(i).getNewMail(), newoffered.get(i).getNewFees());
-		offered.add(a);
-=======
-		InfoMail object = new InfoMail(namesOfCustomersForGifts, mailsOfCustomersForGifts, namesOfProductsAsGifts);
-		return object;
->>>>>>> 7369311192bca5c0bec635d85e50d1a80b86f5fe
-	}
+		
+		/**
+		 * 
+		 */
+		public void updateOfferedInDataBase(String name, String mail) {
+			String url = "jdbc:sqlserver://195.251.249.161:1433;"
+					+ "databaseName=DB29;user=G529;password=59w495f49;";
+			Connection dbcon;
+			Statement stmt;
+			ResultSet rs;
 
+			/** declare ODBC connectivity */
+			try {
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			} catch (java.lang.ClassNotFoundException e) {
+				System.out.print("ClassNotFoundException: ");
+				System.out.println(e.getMessage());
+			}
+
+			/** execute SQL statements */
+			try {
+				dbcon = DriverManager.getConnection(url);
+				stmt = dbcon.createStatement();
+				String sql = "INSERT INTO Java_Offered VALUES('"+name+"','"+mail+"');";
+				stmt.executeUpdate(sql);
+				stmt.close();
+				dbcon.close();
+			} catch (SQLException e) {
+				System.out.print("SQLException: ");
+				System.out.println(e.getMessage());
+			}
+
+		}
+		public Gifts() {
+			
+		}
 }
-
