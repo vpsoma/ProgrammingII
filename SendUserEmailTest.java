b884159e2fb6145f502e8dd7ecb5f5383785e870
Katerina Dimatou <katerina.dimatou@gmail.com>
2020-01-14 00:16:39 +0200

    SendUserEmailTest corrections

--- SendUserEmailTest.java
+++ SendUserEmailTest.java
@@ -2,22 +2,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-
 class SendUserEmailTest {
 
 	@Test
 	public void test() throws Exception {
-		String mails [] = {"katerina.dimatou@gmail.com", "irianna_pap@hotmail.gr"};
-		String names [] = {"Katerina Dimatou", "Eirianna Papoutsi"};
-		String prods [] = {"Monopoly", "Jenga"};
-		
-		InfoMail a = new InfoMail(mails,names,prods);
-		
-		String mailOfUser = "irianna_pap@hotmail.gr";
-		String nameOfUser = "Eirianna Papoutsi";
-		
-		SendUserEmail.sendUserMail(nameOfUser, mailOfUser, a);
-	}
+		String mails[] = { "katerina.dimatou@gmail.com", "irianna_pap@hotmail.gr" };
+		String names[] = { "Katerina Dimatou", "Eirianna Papoutsi" };
+		String prods[] = { "Monopoly", "Jenga" };
 
+		InfoMail a = new InfoMail(mails, names, prods);
+
+		String mailOfUser = "katerina.dimatou@gmail.com";
+		String nameOfUser = "Katerina Dimatou";
+
+		SendUserEmail b = new SendUserEmail();
+
+		b.sendUserMail(nameOfUser, mailOfUser, a);
+	}
 
 }
