4a3ffc8d9cb0400738dd729f6fa9614da603d533
Katerina Dimatou <katerina.dimatou@gmail.com>
2020-01-13 21:21:22 +0200

    Creation of Test for SendEmail class

---
+++ SendEmailTest.java
@@ -0,0 +1,21 @@
+package com.progII.gifthub;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SendEmailTest {
+
+	@Test
+	public void test() throws Exception {
+		String mails [] = {"katerina.dimatou@gmail@gmail.com"};
+		String names [] = {"Katerina Dimatou"};
+		String prods [] = {"Monopoly"};
+		
+		InfoMail a = new InfoMail(mails,names,prods);
+		
+		SendEmail.sendMail(a);
+	}
+
+}
+
+
